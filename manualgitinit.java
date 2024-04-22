ee712050f3c04225ca2e652e7b777a5c22a79ae8
unknown <jgonzalo.hernandezy@gmail.com>
2024-04-21 22:28:20 -0600

    Manuales

---
+++ manualgitinit.java
@@ -0,0 +1,3 @@
+El comando git init crea un nuevo repositorio de Git.
+Puede utilizarse para convertir un proyecto existente y sin versión en un repositorio de Git,
+o para inicializar un nuevo repositorio vacío.
